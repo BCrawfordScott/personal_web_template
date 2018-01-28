8ce8623140258fb0b80e926af24a4121fc6b7922
BCrawfordScott <BrianCrawfordScott@gmail.com>
2018-01-28 16:05:42 -0500

    Initial commit

---
+++ frontend/carl_james.jsx
@@ -0,0 +1,9 @@
+// frontend/carl_james.jsx
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Welcome to Carl James</h1>, root);
+});
